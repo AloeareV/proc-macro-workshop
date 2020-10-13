370cdf1082bc6d49464ad7449d0c565fb6a6079d
Gygaxis Vainhardt <gygaxis@zingolabs.com>
2020-10-13 13:48:41 -0600

    Passes test 01

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,10 +1,14 @@
-extern crate proc_macro;
-
 use proc_macro::TokenStream;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = syn::parse_macro_input!(input as syn::DeriveInput);
+    let name = input.ident;
+    let fields = match input.data {
+        syn::Data::Struct(s) => s,
+        _ => panic!("Can only derive builder for structs"),
+    };
 
-    unimplemented!()
+    println!("{:#?}", input);
+    TokenStream::new()
 }
